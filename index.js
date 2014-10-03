6b3b91976afbe0b6b0aae1c793dca92405301fda
Tong <temberature@gmail.com>
2014-10-03 15:46:35 +0800

    Added read index.html

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+var fs = require('fs');
 var express = require('express')
 var app = express();
+
+var msg = fs.readFileSync('index.html').toString() ;
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(msg)
 })
 
 app.listen(app.get('port'), function() {
